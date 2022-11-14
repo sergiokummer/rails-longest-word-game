1ce8aa617de8d6d7346d22e9ffcfbb40c48f23ff
Sergio Kummer <sergiokummer96@gmail.com>
2022-11-14 17:16:01 -0300

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << ("A".."Z").to_a.sample }
+    return @letters
+  end
+
+  def english_word
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer.downcase}"
+    word_dictionary = URI.open(url).read
+    word = JSON.parse(word_dictionary)
+    return word
+  end
+
+  def grids
+    @answer.chars.sort.all? { |letter| @grid.include?(letter) }
+  end
+
+  def score
+    @grid = params[:grid]
+    @answer = params[:word]
+    grid_letters = @grid.each_char { |letter| print letter, ''}
+    if !grids
+      @result = "Sorry, but #{@answer.upcase} cant be built out of #{grid_letters}."
+    elsif !english_word
+      @result = "Sorry but #{@answer.upcase} does not seem to be an English word."
+    elsif grids && !english_word
+      @result = "Sorry but #{@answer.upcase} does not seem to be an English word."
+    else grids && english_word
+      @result = "Congratulation! #{@answer.upcase} is a valid English word."
+
+    end
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const src = document.getElementsByClassName("source");
+const dest = document.getElementById("dest");
+
+for (let i = 0; i < src.length; i++) {
+  src[i].addEventListener("click", e =>
+    dest.value += "" + src[i].innerText
+  );
+}
